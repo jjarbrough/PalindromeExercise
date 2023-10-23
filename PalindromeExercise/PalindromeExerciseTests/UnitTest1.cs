cefb221780c9352894d8d1306cec32d02a09ce5a
James Yarbrough <jjarbrough@gmail.com>
2023-10-23 11:30:00 -0500

    finished the exercise

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
+using System.Linq.Expressions;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("james", false)]
+        [InlineData("kayak", true)]
+        [InlineData("jim", false)]
+        [InlineData("rotator", true)]
 
+        public void IsAPalindromeTest(string wordToCheck, bool expected)
+        {
+            //Arrange
+            var shakespeare = new WordSmith();
+
+            //Act
+            var actual = shakespeare.IsAPalindrome(wordToCheck);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
